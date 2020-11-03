4b4347e52588ac9ecd7ee9095d7b2bef62b9db6b
EzFroze <sultanbekov-01@yandex.ru>
2020-11-03 17:57:11 +0300

    Главный компонент

---
+++ src/components/RaspDmami.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from "react"
+import Input from "./Input/Input";
+import Tables from "./Tables/Tables"
+import cls from "./RaspDmami.module.scss"
+import "normalize.css"
+
+function RaspDmami() {
+
+  const [data, setData] = useState([])
+  const [changeGroup, setChangeGroup] = useState({
+    grid: ""
+  })
+
+  useEffect(() => {
+    getData()
+      .then(res => res.json())
+      .then((response) => {
+        setData(response.contents)
+      })
+      .catch(e => {
+        console.error(e)
+      })
+  }, [])
+
+  const getData = async () => await fetch("https://rasp.dmami.ru/semester.json")
+
+  const handleEnter = (e) => {
+    if (!data.length) return
+    if (e.key === "Enter")
+      findChangeGroup(e.target.value)
+  }
+
+  const findChangeGroup = (value) => {
+    const group = data.find(d => d.group.title === value) || {}
+    setChangeGroup(group)
+  }
+
+  return (
+    <div className={cls.content}>
+      <h1>РАСПИСАНИЕ МОСКОВСКОГО ПОЛИТЕХА</h1>
+      <Input handleEnter={handleEnter} />
+      <Tables timeTable={changeGroup.grid} />
+    </div>
+  )
+}
+
+export default RaspDmami
